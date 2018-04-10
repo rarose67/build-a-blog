a06f650abc3a4fa86552c1326eea5e302f399f42
rarose67 <robertrosestl67@gmail.com>
2018-04-10 09:53:39 -0500

    add comments and update css

--- main.py
+++ main.py
@@ -13,10 +13,12 @@ db = SQLAlchemy(app)
 
 class Blog(db.Model):
     
+    #Define fields in the Blog table 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(500))
-
+    
+    #Constructor for the Blog class
     def __init__(self, title, body):
         self.title = title
         self.body = body
@@ -27,24 +29,28 @@ def index():
 
     return redirect("/blog")
 
+#route for displaying a list of blog entries
 @app.route('/blog')
 def blog_list():
     #Retrieve query arguments for the url if the user was redirected here.
     blog_id = request.args.get("id")
 
+    # if the url contain query arguments, display the log entry associated with the id 
+    # in the query argument
     if(blog_id):
         blog_id = int(blog_id)
         blogs = Blog.query.filter_by(id=blog_id).all()
-        return render_template('blog-entry.html',title="Blog Entry", blogs=blogs)
+        return render_template('blog-entry.html', title="Blog Entry", blogs=blogs)
     else:
-        #show blog entries in asending order 
+        #show all blog entries in asending order 
         #blogs = Blog.query.all()
 
-        #show blog entries in desending order 
+        #show all blog entries in desending order 
         blogs = Blog.query.order_by((desc(Blog.id))).all()
 
-        return render_template('blog-listing.html',title="Blog List", blogs=blogs)
+        return render_template('blog-listing.html', title="Blog List", blogs=blogs)
 
+#route for displaying the form to add a new blog entry.
 @app.route('/new-post')
 def new_post():
     #Retrieve query arguments for the url if the user was redirected here.
@@ -53,19 +59,21 @@ def new_post():
     terror = request.args.get("terror")
     berror = request.args.get("berror")
 
-     #If the title and body aren't sent as query parameters the previous statements set to None.
+    #If the title and body aren't sent as query parameters the previous statements set to None.
     #In that case, they need to be set to empty strings.
     if btitle == None:
         btitle = ""
     if text == None:
         text = ""
 
-    #display the signup form.
+    #display the new-post form.
     return render_template("new-post.html", title="Add a new Blog entry", blog_title=btitle, blog_post=text, terror=terror, berror=berror)
 
+#route for processing the form to add a new blog entry.
 @app.route('/add-entry', methods=['POST'])
 def add_entry():
 
+    #get info from the form.
     blog_name = request.form['blog_title']
     blog_text = request.form['blog_text']
 
@@ -75,13 +83,13 @@ def add_entry():
     error_query = ""
     
     if (blog_name == ""):
-        # the user tried to enter an invald blog title
-        # so we redirect back to the front page and tell them what went wrong
+        # the user tried to enter a blank blog title
+        # so we redirect back to the form page and tell them what went wrong
         title_error = "Please enter title for your blog entry."
     
     if (blog_text == ""):
         # the user tried to enter a blank post body,
-        # so we redirect back to the front page and tell them what went wrong
+        # so we redirect back to the form page and tell them what went wrong
         body_error = "Please enter the body of your blog entry."
 
     if (title_error != ""):
@@ -95,14 +103,18 @@ def add_entry():
         return redirect("/new-post?btitle=" + blog_name + "&bbody=" + blog_text + error_query)
     else:    
         # if we didn't redirect by now, then all is well
+
+        #Add new blog entry to the database
         new_blog = Blog(blog_name, blog_text)
         db.session.add(new_blog)
         db.session.commit()
         
         #Use Case 1
+        #return to the list of all blog entries
         #return redirect("/blog")
 
-        #Use Case 2 
+        #Use Case 2
+        #Show the new blog entry
         blogs = Blog.query.filter_by(id=new_blog.id).all()
         return render_template('blog-entry.html',title="Blog Entry", blogs=blogs)
 
